45e0f46706ee062e4aedaae6b747b37845043f17
Developer <massssss@gmail.com>
2025-09-22 15:52:49 +0530

    modify env

--- Backend/routes/emailappointmentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const appointmentController = require("../controllers/appointmentController");
-
-// PUT /api/emailappointments/:id/status
-router.put("/:id/status", appointmentController.updateStatus);
-
-module.exports = router;
